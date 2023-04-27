cde5c3dc0459737cb4aa4cc3665aece3f3875385
thearyadev <personal@aryankothari.dev>
2023-04-27 14:00:54 -0400

    Add new get_seasons using season_info table

--- database/database.py
+++ database/database.py
@@ -1,6 +1,7 @@
 import sqlite3
 import leaderboards
 import threading
+import datetime
 
 lock = threading.Lock()
 
@@ -11,7 +12,8 @@ class DatabaseAccess:
         self.cursor = self.connection.cursor()
 
     def create_season(self, seasonNumber: str):
-        self.cursor.execute(f"""CREATE TABLE season_{seasonNumber}(
+        self.cursor.execute(
+            f"""CREATE TABLE season_{seasonNumber}(
                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                                     region TEXT,
                                     role TEXT,
@@ -20,10 +22,12 @@ class DatabaseAccess:
                                     secondMostPlayed TEXT,
                                     thirdMostPlayed TEXT
                             )"""
-                            )
+        )
         self.connection.commit()
 
-    def add_leaderboard_entry(self, seasonNumber: str, leaderboard_entry: leaderboards.LeaderboardEntry):
+    def add_leaderboard_entry(
+        self, seasonNumber: str, leaderboard_entry: leaderboards.LeaderboardEntry
+    ):
         lock.acquire()
         self.cursor.execute(
             f"""
@@ -31,8 +35,14 @@ class DatabaseAccess:
             (region, role, gamesPlayed, firstMostPlayed, secondMostPlayed, thirdMostPlayed)
             VALUES(?, ?, ?, ?, ?, ?)
             """,
-            (leaderboard_entry.region, leaderboard_entry.role, leaderboard_entry.games,
-             leaderboard_entry.heroes[0].name, leaderboard_entry.heroes[1].name, leaderboard_entry.heroes[2].name,)
+            (
+                leaderboard_entry.region,
+                leaderboard_entry.role,
+                leaderboard_entry.games,
+                leaderboard_entry.heroes[0].name,
+                leaderboard_entry.heroes[1].name,
+                leaderboard_entry.heroes[2].name,
+            ),
         )
         self.connection.commit()
         lock.release()
@@ -46,23 +56,45 @@ class DatabaseAccess:
         for line in data:
             results.append(
                 leaderboards.LeaderboardEntry(
-                    heroes=[line[4], line[5], line[6], ],
+                    heroes=[
+                        line[4],
+                        line[5],
+                        line[6],
+                    ],
                     role=leaderboards.Role[line[2]],
                     region=leaderboards.Region[line[1]],
-                    games=line[3]
+                    games=line[3],
                 )
             )
 
         return results
 
     def get_seasons(self) -> list[str]:
+        # lock.acquire()
+        # self.cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table'")
+        # tables = self.cursor.fetchall()
+        # lock.release()
+        # return [t[0].replace("season_", "") for t in tables][1:]
         lock.acquire()
-        self.cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table'")
-        tables = self.cursor.fetchall()
+        self.cursor.execute("SELECT * FROM season_info")
+        data: list[tuple[str, int]] = self.cursor.fetchall()
         lock.release()
-        return [t[0].replace("season_", "") for t in tables][1:]
+        return sorted(
+            [entry[0].replace("season_", "") for entry in data],
+            key=lambda x: (int(x.split("_")[0]), int(x.split("_")[1])),
+        )
 
-    def get_total_hero_occurrence_count(self, hero: str, region: leaderboards.Region, seasonNumber: str) -> int:
+    def get_season_datetime(self, seasonNumber: str) -> datetime.datetime:
+        lock.acquire()
+        self.cursor.execute(
+            f"SELECT collection_date FROM season_info WHERE id = 'season_{seasonNumber}'"
+        )
+        lock.release()
+        return datetime.datetime.fromtimestamp(self.cursor.fetchone()[0])
+
+    def get_total_hero_occurrence_count(
+        self, hero: str, region: leaderboards.Region, seasonNumber: str
+    ) -> int:
         lock.acquire()
         self.cursor.execute(
             f"""
@@ -78,5 +110,5 @@ class DatabaseAccess:
         return result[0]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dba = DatabaseAccess("../data/data.db")
