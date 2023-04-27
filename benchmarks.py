0cf90e32b26105196aa22793c04bf94947a54d84
thearyadev <personal@aryankothari.dev>
2023-04-27 12:06:26 -0400

    start benchmarks file

---
+++ benchmarks.py
@@ -0,0 +1,14 @@
+from rich import print
+import leaderboards
+import heroes
+
+
+answers = {"img1": [["Ana", "Kiriko", "Baptiste"], ["Ana", "Kiriko", "Baptiste"]]}
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
