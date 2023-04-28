8aa170823a8198d7ed29b7a1ded4725b3167c5fa
thearyadev <personal@aryankothari.dev>
2023-04-28 00:19:12 -0400

    formatting

--- generator.py
+++ generator.py
@@ -23,29 +23,27 @@ queue = Queue()
 def worker():
     while True:
         file = queue.get(block=True, timeout=1)
-        print(f"{queue.qsize()} remaining", end='\r')
+        print(f"{queue.qsize()} remaining", end="\r")
         role, region, _ = file.split("-")
         results = leaderboards.parse(
             region=region,
             role=role,
             image_path=f"./assets/leaderboard_images/{file}",
             assets_path="./assets/hero_images",
-            temp_directory="G:/temp"
+            temp_directory="G:/temp",
         )
         for i in results:
             dba.add_leaderboard_entry(seasonNumber=target_season, leaderboard_entry=i)
         queue.task_done()
 
 
-if __name__ == '__main__':
-    pytesseract.pytesseract.tesseract_cmd = \
-        r"G:\autop\Desktop\Estudio\Proyectos\Overwatch Picker\Top500\top500-aggregator\bin\tesseract\tesseract.exe"
+if __name__ == "__main__":
+    pytesseract.pytesseract.tesseract_cmd = r"G:\autop\Desktop\Estudio\Proyectos\Overwatch Picker\Top500\top500-aggregator\bin\tesseract\tesseract.exe"
 
     lb_images = os.listdir("./assets/leaderboard_images")
     for item in lb_images:
         queue.put(item)
     for _ in range(7):
         threading.Thread(target=worker).start()
-    #threading.Thread(target=worker).start()
+    # threading.Thread(target=worker).start()
     queue.join()
-
--- neural_network/__init__.py
+++ neural_network/__init__.py
@@ -1,3 +1,10 @@
-from .model import (Accuracy_Categorical, Activation_ReLU, Activation_Softmax,
-                    Layer_Dense, Loss_CategoricalCrossentropy, Model,
-                    Optimizer_Adam, create_data_mnist)
+from .model import (
+    Accuracy_Categorical,
+    Activation_ReLU,
+    Activation_Softmax,
+    Layer_Dense,
+    Loss_CategoricalCrossentropy,
+    Model,
+    Optimizer_Adam,
+    create_data_mnist,
+)
