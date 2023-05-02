0c892e048e2a077f045ac26fbd1d26ebbb2eaba9
thearyadev <personal@aryankothari.dev>
2023-05-02 02:19:32 -0400

    temp file changes to test imports

--- srcfile_duplicator.py
+++ srcfile_duplicator.py
@@ -2,8 +2,14 @@ import os
 import shutil
 import uuid
 
-src = "./assets/hero_images/LifeWeaver.png"
-dst = "./assets/top_500_unprocessed_images/train/38/"
+# src = "./assets/hero_images/LifeWeaver.png"
+# dst = "./assets/top_500_unprocessed_images/train/38/"
 
-for i in range(108):
-    shutil.copyfile(src, dst + uuid.uuid4().hex + ".png")
+# for i in range(108):
+#     shutil.copyfile(src, dst + uuid.uuid4().hex + ".png")
+
+import importlib
+
+package = importlib.import_module("models.thearyadev-2023-04-30")
+
+print(getattr(package, "Model"))
