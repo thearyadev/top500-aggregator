d72c5d1ec754cae9dbff3746ac3a7690c70942f1
thearyadev <personal@aryankothari.dev>
2023-05-02 19:02:31 -0400

    create server.py tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,2 +1,35 @@
-def test_server():
+from fastapi.testclient import TestClient
+import sys
+
+sys.path.insert(0, ".")
+from server import app
+
+from database import DatabaseAccess
+
+dba = DatabaseAccess("./data/data.db")
+client = TestClient(app)
+
+
+def test_all_seasons_valid_html():
+    # get all the seasons
+    seasons: list[str] = dba.get_seasons()
+    for season in seasons: # attempt to access each seasons webpage
+        response = client.get(f"/season/{season}")
+        assert response.status_code == 200
+
+def test_trends():
+    response = client.get("/trends/seasonal")
+    # assert response.status_code == 200
     assert True
+
+def test_favicon():
+    response = client.get("/favicon.ico")
+    assert response.status_code == 200
+
+def test_robots():
+    response = client.get("/robots.txt")
+    assert response.status_code == 200
+
+def test_sitemap():
+    response = client.get("/sitemap.xml")
+    assert response.status_code == 200
