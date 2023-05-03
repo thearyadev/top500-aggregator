f4c0642036661b2a5224f101c4510af8dde4fa53
thearyadev <personal@aryankothari.dev>
2023-05-03 14:12:33 -0400

    create statistics tests

---
+++ tests/test_stat_calculations.py
@@ -0,0 +1,69 @@
+from statistic import (
+    get_hero_trends,
+    get_mean,
+    get_number_of_ohp,
+    get_number_of_thp,
+    get_occurrences,
+    get_occurrences_most_played,
+    get_stdev,
+    get_variance,
+)
+from database import DatabaseAccess
+from leaderboards import Region, Role
+
+dba = DatabaseAccess("./data/data.db")
+data = []
+
+for season in dba.get_seasons():
+    data.append(dba.get_all_records(seasonNumber=season))
+
+
+def test_get_occurrences():
+    for season_data in data:
+        for region in [Region.AMERICAS, Region.EUROPE, Region.ASIA]:
+            assert get_occurrences(data=season_data, region=region)
+
+def test_get_occurrences_most_played():
+    for season_data in data:
+        for region in [Region.AMERICAS, Region.EUROPE, Region.ASIA]:
+            for role in [Role.DAMAGE, Role.SUPPORT, Role.TANK]:
+                for most_played_slot in (1, 2, 3):
+                    assert get_occurrences_most_played(data=season_data, region=region, role=role, mostPlayedSlot=most_played_slot)
+
+def test_get_mean():
+    for season_data in data:
+        for region in [Region.AMERICAS, Region.EUROPE, Region.ASIA]:
+            for role in [Role.DAMAGE, Role.SUPPORT, Role.TANK]:
+                for most_played_slot in (1, 2, 3):
+                    assert get_mean(data=get_occurrences_most_played(data=season_data, region=region, role=role, mostPlayedSlot=most_played_slot))
+                
+                assert get_mean(data=get_occurrences(data=season_data, region=region))
+
+def test_get_variance():
+    for season_data in data:
+        for region in [Region.AMERICAS, Region.EUROPE, Region.ASIA]:
+            for role in [Role.DAMAGE, Role.SUPPORT, Role.TANK]:
+                for most_played_slot in (1, 2, 3):
+                    assert get_variance(data=get_occurrences_most_played(data=season_data, region=region, role=role, mostPlayedSlot=most_played_slot))
+                
+                assert get_variance(data=get_occurrences(data=season_data, region=region))
+
+def test_get_stdev():
+    for season_data in data:
+        for region in [Region.AMERICAS, Region.EUROPE, Region.ASIA]:
+            for role in [Role.DAMAGE, Role.SUPPORT, Role.TANK]:
+                for most_played_slot in (1, 2, 3):
+                    assert get_stdev(data=get_occurrences_most_played(data=season_data, region=region, role=role, mostPlayedSlot=most_played_slot))
+                
+                assert get_stdev(data=get_occurrences(data=season_data, region=region))
+
+def test_get_hero_trends():
+    assert get_hero_trends(dba)
+
+def test_get_number_of_ohp():
+    for season_data in data:
+        assert get_number_of_ohp(season_data)
+
+def test_get_number_of_thp():
+    for season_data in data:
+        assert get_number_of_thp(season_data)
