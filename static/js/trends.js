e1e13cbddb72c3495c9a40bec90e3a73056f3b7d
thearyadev <personal@aryankothari.dev>
2023-05-11 19:04:00 -0400

    minor graph changes

--- static/js/trends.js
+++ static/js/trends.js
@@ -1,12 +1,12 @@
 const data = $(`data`).data().trends;
 console.log(data)
 const charts = [
-    "SUPPORT", 
-    "DAMAGE", 
+    "SUPPORT",
+    "DAMAGE",
     "TANK",
 ]
 
-google.charts.load("current", {"packages": ['corechart']})
+google.charts.load("current", { "packages": ['corechart'] })
 google.charts.setOnLoadCallback(function () {
     charts.forEach(drawChart)
 })
@@ -18,25 +18,25 @@ function drawChart(chartName) { // value is region. Americas, Europe, Asia, All
         ["Season"]
     ];
 
-    for (let season in data){
+
+    for (let season in data) {
         // for each season
-        
+
         let specific_chart_data = data[season][chartName]
-        let season_data_to_add = [season, ]
-        for (let hero in specific_chart_data){
-            if (!graphData[0].includes(data[season][chartName][hero]["hero"])){
+        let season_data_to_add = ["S" + season.replace("_8", ""),]
+        for (let hero in specific_chart_data) {
+            if (!graphData[0].includes(data[season][chartName][hero]["hero"])) {
                 graphData[0].push(data[season][chartName][hero]["hero"])
             }
-            
-            
-            
+
+
+
             season_data_to_add.push(data[season][chartName][hero]["count"])
         }
         graphData.push(season_data_to_add)
 
     }
 
-    console.log(graphData)
     var parsedGraphData = google.visualization.arrayToDataTable(graphData)
 
     var chart = new google.visualization.LineChart(document.getElementById(chartName));
@@ -47,7 +47,12 @@ function drawChart(chartName) { // value is region. Americas, Europe, Asia, All
             'height': '90%'
         },
         isStacked: "relative",
+        theme: "material",
+        fontName: 'Lato',
         
+
+
+
 
     }
     chart.draw(parsedGraphData, options)
@@ -55,10 +60,20 @@ function drawChart(chartName) { // value is region. Americas, Europe, Asia, All
 
 
 
-function get_heroes(obj){
+function get_heroes(obj) {
     const heroes = []
-    for (let hero of obj){
+    for (let hero of obj) {
         heroes.push(hero.hero)
     }
     return heroes
 }
+
+
+window.addEventListener('resize', function () { // redraw charts on resize
+    
+    charts.forEach((value) => {
+        $(`#${value}`).empty()
+    })
+
+    charts.forEach(drawChart)
+});
