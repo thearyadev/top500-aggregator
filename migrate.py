3877fd376f2cbe89e10ec7e77c8306a201a00958
thearyadev <personal@aryankothari.dev>
2023-08-21 14:59:55 -0400

    add dotenv dependency, configure new db connection in migration script

--- migrate.py
+++ migrate.py
@@ -1,5 +1,21 @@
 import database
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 db = database.DatabaseAccess("./data/data.db")
-print(len(db.get_all_records("5_8")))
+# print(len(db.get_all_records("5_8")))
+tdb = mysql.connector.connect(
+    host=os.getenv("MYSQLHOST"),
+    user=os.getenv("MYSQLUSER"),
+    password=os.getenv("MYSQLPASSWORD"),
+    database=os.getenv("MYSQLDATABASE"),
+    port=os.getenv("MYSQLPORT"),
+)
+
+cursor = tdb.cursor()
+cursor.execute("SELECT * FROM season_info")
+
+print(cursor.fetchall())
