173e7fa4a6b0a13b73a88e90ef1251c59ad1a084
thearyadev <personal@aryankothari.dev>
2023-08-25 14:10:30 -0400

    update srcfile duplicator

--- srcfile_duplicator.py
+++ srcfile_duplicator.py
@@ -3,14 +3,12 @@ import os
 import shutil
 import uuid
 
-# src = "./assets/hero_images/LifeWeaver.png"
-# dst = "./assets/top_500_unprocessed_images/train/38/"
+src = "./assets/hero_images/Illari.png"
+train_dst = "./assets/top_500_unprocessed_images/train/39/"  # ensure these directories are created
+test_dst = "./assets/top_500_unprocessed_images/test/39/"  # ensure these directories are created
 
-# for i in range(108):
-#     shutil.copyfile(src, dst + uuid.uuid4().hex + ".png")
+for i in range(108):
+    shutil.copyfile(src, train_dst + uuid.uuid4().hex + ".png")
 
-
-package = importlib.import_module("models.thearyadev-2023-04-30")
-
-print(getattr(package, "Model"))
-## lol.
+for i in range(27):
+    shutil.copyfile(src, test_dst + uuid.uuid4().hex + ".png")
