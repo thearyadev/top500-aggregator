49618e089c0bd9df0fa8eefac10b8ddd67244944
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2023-12-23 13:41:05 -0500

    88 ssr frontend framework (#89)
    * update gitignore
    
    * initialize nextjs app
    
    * initial project scaffold
    
    * conditionally use body wrapping (grid)
    
    * rename
    
    * remap prop to better represent data
    
    * fetch serverside props and populate charts
    
    * fetch serverside props and populate charts
    
    * remove hr
    
    * add line chart
    
    * remove season list
    
    * load season list on each page
    
    * populate trends chart
    
    * ensure cap case is correct for all titles
    
    * Change chart title for O_ALL charts
    
    * remove unused components
    
    * Disable workflows (temporarily)
    
    * update dockerignore
    
    * add eslint exclusions
    
    * change endpoints to docker internal names
    
    * rename dockerfile to dockerfile.server
    
    * create nextjs dockerfile
    
    * create testing docker-compose.yml
    
    * change web to server
    
    * change web to server
    
    * fix dependency name
    
    * fix dependency name
    
    * add navbar links
    
    * remove unused data prop
    
    * bun lockfile
    
    * modify header condition
    
    * populate latest season in index page
    
    * add frontend buildable workflow
    
    * remove season 8
    
    * add/modify ghcr labels
    
    * add push logic
    
    * enable workflows
    
    * fix dockerfile reference
    
    * disable tests again
    
    * remove test pass condition (temporary)
    
    * run push on main branch only
    
    * remove server tests (will be re-written)
    
    * revert workflow conditions

---
+++ frontend/app/components/index.ts
@@ -0,0 +1,4 @@
+export {default as Header} from "./header/header"
+export {default as Card} from "./card/card"
+export {default as BarChart} from "./charts/barChart"
+export {default as LineChart} from "./charts/lineChart"
