ba17f45c94e38a7821c46a55bca5d5b5a250477a
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2023-12-24 14:27:57 -0500

    real tests (#95)
    * delete test
    
    * add actual database tests
    
    * database connection fixture
    
    * code formatting
    
    * add additional conditions for publish
    
    * format
    
    * add stats test (incomplete: trends)
    
    * add trends child test
    
    * mypy types

--- heroes/hero_comparison.py
+++ heroes/hero_comparison.py
@@ -64,7 +64,7 @@ class Heroes:
             37: "Zenyatta",
             38: "LifeWeaver",
             39: "Illari",
-            40: "Mauga"
+            40: "Mauga",
         }
         self.hero_role: dict[str, str] = {
             "Ana": "SUPPORT",
@@ -107,7 +107,7 @@ class Heroes:
             "Zarya": "TANK",
             "Zenyatta": "SUPPORT",
             "LifeWeaver": "SUPPORT",
-            "Mauga": "TANK"
+            "Mauga": "TANK",
         }
 
         self.hero_colors: dict[str, str] = {
@@ -150,7 +150,7 @@ class Heroes:
             "Zarya": "#F291BB",
             "Zenyatta": "#F5EC91",
             "LifeWeaver": "#E0B6C5",
-            "Mauga": "#E0B6C5"
+            "Mauga": "#E0B6C5",
         }
 
     def predict_hero_name(self, image_data: np.ndarray, model_name: str) -> Hero:
