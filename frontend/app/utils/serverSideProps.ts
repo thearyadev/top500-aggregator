fea2166d6229c203187a858ec5ffc1924900aa72
Aryan Kothari <personal@aryankothari.dev>
2023-12-26 04:54:23 +0000

    move types and fetches to different file

---
+++ frontend/app/utils/serverSideProps.ts
@@ -0,0 +1,59 @@
+const mode = process.env.NODE_ENV;
+const backendUrl = mode === "development" ? "http://localhost:7771" : "http://server:8000"
+export type Statistic = {
+    mean: number;
+    standard_deviation: number;
+    variance: number;
+}
+
+export type BarChartData = {
+    labels: string[];
+    values: number[];
+}
+
+export type SingleChart = {
+    graph: BarChartData;
+    statistic: Statistic;
+}
+
+export type SeasonData = {
+    [key: string]: SingleChart
+}
+
+export type StdDevs = {
+    DAMAGE: number;
+    SUPPORT: number;
+    TANK: number;
+}
+
+export type TrendLine = {
+    name: string;
+    data: number[]
+}
+
+export async function fetchSingleSeasonPageChartData(seasonNumber: number): Promise<SingleChart[]> {
+    const response = await fetch(`${backendUrl}/chart/${seasonNumber}_8`);
+    return await response.json();
+}
+
+
+export async function fetchSeasonList(): Promise<string[]> {
+    const response = await fetch(`${backendUrl}/d/seasons`)
+    return await response.json()
+}
+
+
+export async function fetchSingleSeasonStdDevs(seasonNumber: number): Promise<StdDevs> {
+    const response = await fetch(`${backendUrl}/d/single_season_std_by_role/${seasonNumber}_8`)
+    return await response.json()
+}
+
+export async function fetchSeasonalOccurrenceTrend(): Promise<TrendLine[]>{
+    const response = await fetch(`${backendUrl}/chart/trend/d`)
+    return await response.json()
+}
+
+export async function fetchSeasonalStdDevTrendByRole(): Promise<TrendLine[]>{
+    const response = await fetch(`${backendUrl}/d/all_seasons_std_by_role`)
+    return await response.json()
+}
--- frontend/pages/season/[seasonNumber].tsx
+++ frontend/pages/season/[seasonNumber].tsx
@@ -2,32 +2,8 @@ import {useRouter} from "next/router";
 import {Card} from "@/app/components";
 import {BarChart} from "@/app/components";
 import {GetServerSidePropsContext} from "next";
-
-type Statistic = {
-    mean: number;
-    standard_deviation: number;
-    variance: number;
-}
-
-type BarChartData = {
-    labels: string[];
-    values: number[];
-}
-
-type SingleChart = {
-    graph: BarChartData;
-    statistic: Statistic;
-}
-
-type SeasonData = {
-    [key: string]: SingleChart;
-}
-
-type StdDevs = {
-    DAMAGE: number;
-    SUPPORT: number;
-    TANK: number;
-}
+import type {SeasonData, StdDevs} from "@/app/utils/serverSideProps";
+import {fetchSingleSeasonStdDevs, fetchSingleSeasonPageChartData} from "@/app/utils/serverSideProps";
 
 const HeroStdDev = ({value, role}: {value: number, role: string}) => {
     return (
@@ -39,20 +15,20 @@ const HeroStdDev = ({value, role}: {value: number, role: string}) => {
     )
 }
 
-const Season = ({data, season_list, std_devs}: { data: SeasonData, season_list: string[], std_devs: StdDevs}) => {
+const Season = ({seasonChartData, seasonStdDevs}: { seasonChartData: SeasonData, seasonStdDevs: StdDevs}) => {
     return (
         <>
             <Card title="Role Standard Deviation: All Slots, All Regions" subtitle={"Note: The standard deviation is calculated with the 10th percentile excluded. T500 aggregator by nature skews the accuracy of the low outliers in a data set. For this reason, the bottom 10% of entries for any given set (support, damage or tank) is excluded from the calculation."}>
-                <HeroStdDev value={std_devs.SUPPORT} role={"SUPPORT"} />
-                <HeroStdDev value={std_devs.DAMAGE} role={"DAMAGE"}/>
-                <HeroStdDev value={std_devs.TANK} role={"TANK"}/>
+                <HeroStdDev value={seasonStdDevs.SUPPORT} role={"SUPPORT"} />
+                <HeroStdDev value={seasonStdDevs.DAMAGE} role={"DAMAGE"}/>
+                <HeroStdDev value={seasonStdDevs.TANK} role={"TANK"}/>
             </Card>
 
             <Card title="Hero Occurrences: All Slots" nowrap>
-                {Object.keys(data).map(key => {
+                {Object.keys(seasonChartData).map(key => {
                     if (key.includes("O_ALL")){
                         const [_, role, region] = key.split("_")
-                        return <BarChart title={`${region}`} graph={data[key].graph} maxY={region === "ALL" ? 1250 : 500 } />
+                        return <BarChart title={`${region}`} graph={seasonChartData[key].graph} maxY={region === "ALL" ? 1250 : 500 } />
                     }
                 })}
             </Card>
@@ -60,20 +36,20 @@ const Season = ({data, season_list, std_devs}: { data: SeasonData, season_list: 
 
 
             <Card title="Hero Occurrences: First Most Played">
-                {Object.keys(data).map(key => {
+                {Object.keys(seasonChartData).map(key => {
                     if (key.includes("OFMP")){
                         const [_, role, region] = key.split("_")
-                        return <BarChart title={`${role}: ${region}`} graph={data[key].graph} maxY={region === "ALL" ? 500 : 300 } />
+                        return <BarChart title={`${role}: ${region}`} graph={seasonChartData[key].graph} maxY={region === "ALL" ? 500 : 300 } />
                     }
                 })}
 
             </Card>
 
             <Card title="Hero Occurrences: Second Most Played">
-                {Object.keys(data).map(key => {
+                {Object.keys(seasonChartData).map(key => {
                     if (key.includes("OSMP")){
                         const [_, role, region] = key.split("_")
-                        return <BarChart title={`${role}: ${region}`} graph={data[key].graph} maxY={region === "ALL" ? 500 : 300 } />
+                        return <BarChart title={`${role}: ${region}`} graph={seasonChartData[key].graph} maxY={region === "ALL" ? 500 : 300 } />
                     }
                 })}
 
@@ -81,10 +57,10 @@ const Season = ({data, season_list, std_devs}: { data: SeasonData, season_list: 
 
 
             <Card title="Hero Occurrences: Third Most Played">
-                {Object.keys(data).map(key => {
+                {Object.keys(seasonChartData).map(key => {
                     if (key.includes("OTMP")){
                         const [_, role, region] = key.split("_")
-                        return <BarChart title={`${role}: ${region}`} graph={data[key].graph} maxY={region === "ALL" ? 500 : 300 } />
+                        return <BarChart title={`${role}: ${region}`} graph={seasonChartData[key].graph} maxY={region === "ALL" ? 500 : 300 } />
                     }
                 })}
 
@@ -97,23 +73,13 @@ const Season = ({data, season_list, std_devs}: { data: SeasonData, season_list: 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
     // @ts-ignore
     const {seasonNumber} = context.params;
-
-    // Make an API call using seasonNumber
-    const res = await fetch(`http://server:8000/chart/${seasonNumber}_8`);
-    const data = await res.json();
-
-
-    const res2 = await fetch("http://server:8000/d/seasons")
-    const season_list = await res2.json()
-
-    const res3 = await fetch(`http://server:8000/d/single_season_std_by_role/${seasonNumber}_8`)
-    const std_devs = await res3.json()
+    const seasonChartData = await fetchSingleSeasonPageChartData(seasonNumber)
+    const seasonStdDevs = await fetchSingleSeasonStdDevs(seasonNumber)
 
     return {
         props: {
-            data,
-            season_list,
-            std_devs
+            seasonChartData,
+            seasonStdDevs
         },
     };
 }
