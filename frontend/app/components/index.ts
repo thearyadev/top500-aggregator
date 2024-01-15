0bc5effd3703d85f08f0102c321045867525372a
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-01-15 09:46:35 -0500

    94 add footer (#109)
    * create footer component
    
    * Export footer from index
    
    * add styles and structure to footer
    
    * place footer on page

--- frontend/app/components/index.ts
+++ frontend/app/components/index.ts
@@ -2,3 +2,4 @@ export {default as Header} from "./header/header"
 export {default as Card} from "./card/card"
 export {default as BarChart} from "./charts/barChart"
 export {default as LineChart} from "./charts/lineChart"
+export {default as Footer} from './footer/footer'
