56876f23a5272c222c854448ba8c617c5d8c592a
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-01-23 18:24:27 +0000

    Allow caller provided className for card

--- frontend/app/components/card/card.tsx
+++ frontend/app/components/card/card.tsx
@@ -1,18 +1,24 @@
 import styles from "./card.module.scss";
+import classNames from "classnames";
 
 const Card = ({
   children,
   title,
   subtitle,
   nowrap,
+  className,
 }: {
   children: React.ReactNode;
   title: string;
   subtitle?: string;
   nowrap?: boolean;
+  className?: string | string[]
 }) => {
+
+  const cardStyle = classNames(styles.card, className)
+
   return (
-    <div className={styles.card}>
+    <div className={cardStyle}>
       <div className={styles.headingContainer}>
         <h3 className={styles.title}>{title}</h3>
         {subtitle ? <h4 className={styles.subtitle}>{subtitle}</h4> : null}
