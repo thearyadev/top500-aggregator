521d354b07e2425ee42b541391293dc39dbe91f0
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-01-24 18:45:59 +0000

    Fix formatting with tw 4

--- frontend/app/components/card/card.tsx
+++ frontend/app/components/card/card.tsx
@@ -2,30 +2,33 @@ import styles from "./card.module.scss";
 import classNames from "classnames";
 
 const Card = ({
-  children,
-  title,
-  subtitle,
-  nowrap,
-  className,
+    children,
+    title,
+    subtitle,
+    nowrap,
+    className,
 }: {
-  children: React.ReactNode;
-  title: string;
-  subtitle?: string;
-  nowrap?: boolean;
-  className?: string | string[]
+    children: React.ReactNode;
+    title: string;
+    subtitle?: string;
+    nowrap?: boolean;
+    className?: string | string[];
 }) => {
+    const cardStyle = classNames(styles.card, className);
 
-  const cardStyle = classNames(styles.card, className)
-
-  return (
-    <div className={cardStyle}>
-      <div className={styles.headingContainer}>
-        <h3 className={styles.title}>{title}</h3>
-        {subtitle ? <h4 className={styles.subtitle}>{subtitle}</h4> : null}
-      </div>
-      <div className={nowrap ? undefined : styles.cardBodyWrap}>{children}</div>
-    </div>
-  );
+    return (
+        <div className={cardStyle}>
+            <div className={styles.headingContainer}>
+                <h3 className={styles.title}>{title}</h3>
+                {subtitle ? (
+                    <h4 className={styles.subtitle}>{subtitle}</h4>
+                ) : null}
+            </div>
+            <div className={nowrap ? undefined : styles.cardBodyWrap}>
+                {children}
+            </div>
+        </div>
+    );
 };
 
 export default Card;
--- frontend/app/components/charts/lineChart.tsx
+++ frontend/app/components/charts/lineChart.tsx
@@ -8,75 +8,79 @@ import type { TrendLine } from "@/app/utils/serverSideProps";
 import classNames from "classnames";
 
 interface LineChartProps extends HighchartsReact.Props {
-  title: string;
-  data: TrendLine[];
-  seasons: string[];
-  className: string;
+    title: string;
+    data: TrendLine[];
+    seasons: string[];
+    className: string;
 }
 
 const LineChart = (props: LineChartProps) => {
-  const { data, seasons, title } = props;
-  const options: Highcharts.Options = {
-    title: {
-      // @ts-ignore
-      text: null,
-      margin: 0,
-    },
-    xAxis: {
-      categories: seasons,
-      min: 0,
-      max: 9,
-    },
-    series: data.map((item) => ({
-      color: HeroColors[item.name] ?? null,
-      type: "line",
-      ...item,
-    })),
-    credits: {
-      enabled: false,
-    },
-    yAxis: {
-      title: {
-        text: null,
-      },
-    },
-    plotOptions: {
-      series: {
-        pointPlacement: "on",
-        label: {
-          connectorAllowed: false,
+    const { data, seasons, title } = props;
+    const options: Highcharts.Options = {
+        title: {
+            // @ts-ignore
+            text: null,
+            margin: 0,
         },
-      },
-    },
-    chart: {
-      height: "45%",
-      events:{
-        load: () => {setLoading(false)}
-      }
-    },
-  };
-  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
-  const [loading, setLoading] = useState(true)
-  return (
-    <div className={props.className}>
-      <h5 className="text-center pb-2">{title}</h5>
+        xAxis: {
+            categories: seasons,
+            min: 0,
+            max: 9,
+        },
+        series: data.map((item) => ({
+            color: HeroColors[item.name] ?? null,
+            type: "line",
+            ...item,
+        })),
+        credits: {
+            enabled: false,
+        },
+        yAxis: {
+            title: {
+                text: null,
+            },
+        },
+        plotOptions: {
+            series: {
+                pointPlacement: "on",
+                label: {
+                    connectorAllowed: false,
+                },
+            },
+        },
+        chart: {
+            height: "45%",
+            events: {
+                load: () => {
+                    setLoading(false);
+                },
+            },
+        },
+    };
+    const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+    const [loading, setLoading] = useState(true);
+    return (
+        <div className={props.className}>
+            <h5 className="text-center pb-2">{title}</h5>
 
-      <div className={loading ? "hidden" : ""}>
-        <HighchartsReact
-          id="gnomegnome"
-          highcharts={Highcharts}
-          options={options}
-          ref={chartComponentRef}
-          {...props}
-        />
+            <div className={loading ? "hidden" : ""}>
+                <HighchartsReact
+                    id="gnomegnome"
+                    highcharts={Highcharts}
+                    options={options}
+                    ref={chartComponentRef}
+                    {...props}
+                />
+            </div>
 
-      </div>
-
-      <div role="status" className={`max-w flex text-center justify-center items-center h-[60rem]  animate-pulse ${loading ? '' : "hidden"}`}>
-        <p>Loading...</p>
-      </div>
-    </div>
-  );
+            <div
+                role="status"
+                className={`max-w flex text-center justify-center items-center h-[60rem]  animate-pulse ${loading ? "" : "hidden"}`}
+            >
+                <p>Loading...</p>
+            </div>
+        </div>
+    );
 };
 
 export default LineChart;
