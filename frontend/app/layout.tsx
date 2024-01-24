d62ecd6128335fb20a63657eafefcfac316cb296
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-01-24 18:45:59 +0000

    Fix formatting with tw 4

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,23 +6,23 @@ import { Footer, Header } from "@/app/components";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "T500 Aggregator",
-  description: "T500 Aggregator",
+    title: "T500 Aggregator",
+    description: "T500 Aggregator",
 };
 
 // export const dynamic = "force-dynamic";
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <Header />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
 }
