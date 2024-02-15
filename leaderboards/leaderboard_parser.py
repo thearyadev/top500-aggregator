911591ef42616bc4a1197c2a5b82a0bc4d75a903
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-02-15 17:39:51 -0500

    Add new parser functions; remove legacy

--- leaderboards/leaderboard_parser.py
+++ leaderboards/leaderboard_parser.py
@@ -1,9 +1,14 @@
-import os
 from enum import Enum
+from heroes import Heroes, hero_comparison
+from PIL.Image import Image as ImageType
+from typing import Final 
+from pathlib import Path
 
-import numpy as np
+ROW_HEIGHT_PX: Final[int] = 46
+ROW_SKIP_PX: Final[int] = 13
+COLUMN_WIDTH_PX: Final[int] = 100
+COLUMN_SKIP_PX: Final[int] = 2
 
-from heroes import Hero, Heroes
 
 
 class ByNameEnum(Enum):
@@ -32,20 +37,18 @@ class Role(ByNameEnum):
 class LeaderboardEntry:
     def __init__(
         self,
-        heroes: list[Hero] | list[str],
-        games: int,
+        heroes: list[str],
         region: Region,
         role: Role,
     ):
         self.heroes = heroes
-        self.games = games
         self.region = region
         self.role = role
 
     def __repr__(self):
         return (
             f"LeaderboardEntry(heroes={self.heroes},"
-            f" games_played={self.games}, region={self.region}, role={self.role})"
+            f" region={self.region}, role={self.role})"
         )
 
     def hasHero(self, hero: str) -> bool:
@@ -53,116 +56,65 @@ class LeaderboardEntry:
             return True
         return False
 
+def crop_to_hero_section(pil_image: ImageType) -> ImageType:
+    return pil_image.crop((1392, 294, 1696, 871))
 
-def clear_temp_dir(temp_dir: str) -> None:
-    for f in os.listdir(temp_dir):
-        os.remove(f"{temp_dir}/{f}")
-
-
-def parse(
-    image_path: str,
-    assets_path: str,
-    region: Region,
-    role: Role,
-    model_name: str | None = None,
-) -> list[LeaderboardEntry]:
-    import cv2  # type: ignore
-    from PIL import Image
-
-    results: list[LeaderboardEntry] = list()  # init return array
-    heroComparor = (
-        Heroes()
-    )  # class used to evaluate which hero is currently being looked at
-    image_input: np.ndarray = cv2.imread(image_path)  # read the leaderboard image
-    assert cv2.cvtColor(image_input, cv2.COLOR_BGR2GRAY).shape == (
-        1080,
-        1920,
-    )  # assert the shape as the calculations
-    # below are predefined pixel values
-
-    # defines the box around the first hero in a players top 3 on the lb
-    starting_top_point: list[int] = [1306, 304]
-    starting_bottom_point: list[int] = [1355, 354]
-    # remember the vertical origin of the first hero
-    x_origin: tuple[int, int] = (
-        starting_top_point[0],
-        starting_bottom_point[0],
-    )
-    for leaderboard_entry in range(10):  # 10 leaderboard entries per page
-        heroes_played: list[
-            Hero
-        ] = list()  # list of Hero. Saves the image in case its needed.
-        for single_hero in range(3):  # a player can have 3 most played.
-            if single_hero == 0:  # for the first iter
-                # crop the image to the predefined box.
-                modified: np.ndarray = image_input[
-                    starting_top_point[1] : starting_bottom_point[1],
-                    starting_top_point[0] : starting_bottom_point[0],
-                ]
-
-                if model_name is not None:
-                    heroes_played.append(
-                        Hero(
-                            name=heroComparor.predict_hero_name(
-                                cv2.cvtColor(modified, cv2.COLOR_BGR2GRAY),
-                                model_name=model_name,
-                            ).name,
-                            image=Image.fromarray(modified),
-                        )  # creates and appends hero object to results
-                    )
-                else:
-                    raise Exception("model_name is not defined. Cannot continue.")
-
-                continue
-
-            # moves box to the next hero in the leaderboard entry. This happens two times after the initial box.
-            starting_top_point[0] += 52
-            starting_bottom_point[0] += 52
-            # write the file to the file system.
-            # this is required because passing the cv2 numpy array directly
-            # to the hero comparison caused failed tests
-            # converting cv2 numpy array to PIL image did not work either.
-            # save to filesystem, and let PIL reload it.
-            modified: np.ndarray = image_input[  # type: ignore
-                # type ignored for var redeclaration
-                starting_top_point[1] : starting_bottom_point[1],
-                starting_top_point[0] : starting_bottom_point[0],
-            ]
-            # cv2.imwrite(path := f"{temp_directory}/{uuid.uuid4().hex}.png", modified)
-
-            if model_name is not None:
-                heroes_played.append(
-                    Hero(
-                        name=heroComparor.predict_hero_name(
-                            cv2.cvtColor(modified, cv2.COLOR_BGR2GRAY),
-                            model_name=model_name,
-                        ).name,
-                        image=Image.fromarray(modified),
-                    )  # creates and appends hero object to results
+def crop_split_row(pil_image: ImageType) -> list[ImageType]:
+    current_offset_pos: int = 0
+    o_width, o_height = pil_image.size
+    result: list[ImageType] = list()
+    for _ in range(10):
+        result.append(
+            pil_image.copy().crop(
+                (
+                    0,
+                    current_offset_pos,
+                    o_width,
+                    min(current_offset_pos + ROW_HEIGHT_PX, o_height),
                 )
-            else:
-                raise Exception("model_name is not defined. Cannot continue.")
-
-        # for the games played
-        # move the box back and widen it to fit all the text.
-        starting_top_point[0] -= 400
-        starting_bottom_point[0] -= 250
-
-        results.append(
-            LeaderboardEntry(games=0, heroes=heroes_played, region=region, role=role)
+            )
         )
-        # box start returns to the vertical origin (first hero top left corner)
-        starting_top_point[0] = x_origin[0]
-        starting_bottom_point[0] = x_origin[1]
-        # translate the box down one row (next player in t500)
-        starting_top_point[1] += 55
-        starting_bottom_point[1] += 55
-        # repeat 10 times
+        current_offset_pos += ROW_HEIGHT_PX + ROW_SKIP_PX
+        if current_offset_pos >= o_height:
+            break
 
-    # clear_temp_dir(temp_directory)
+    return result
 
+def crop_split_column(pil_image: ImageType) -> list[ImageType]:
+    current_offset_pos: int = 0
+    result: list[ImageType] = list()
+    o_width, o_height = pil_image.size
+    for _ in range(3):
+        result.append(
+            pil_image.copy().crop(
+                (
+                    current_offset_pos,
+                    0,
+                    min(current_offset_pos + COLUMN_WIDTH_PX, o_width),
+                    o_height
+                )
+            )
+        )
+        current_offset_pos += COLUMN_WIDTH_PX + COLUMN_SKIP_PX
+        if current_offset_pos >= o_width:
+            break
+    return list(reversed(result)) # Hero placement has been reversed in Season 9 (blizzard moment.)
+
+def parse_leaderboard_to_leaderboard_entries(leaderboard_image: ImageType, region: Region, role: Role) -> list[LeaderboardEntry]:
+    hero_comp = Heroes()
+    hero_section = crop_to_hero_section(leaderboard_image)
+    row_entries = crop_split_row(hero_section)
+    split_column_entries = [crop_split_column(row) for row in row_entries]
+    results: list[LeaderboardEntry] = list() 
+    for row in split_column_entries: # each record (10)
+
+        results.append(LeaderboardEntry(
+            heroes=[hero_comp.predict_hero_name(hero_image, Path("./models/tm")) for hero_image in row], 
+            role=role,
+            region=region
+
+        ))
     return results
 
 
-if __name__ == "__main__":
-    ...
+
