fb87c3441c7e402f4f8befbf2c9297d9ddabbb2f
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-02-28 02:10:43 -0500

    rewrite generator. tempfix

--- utils/generator.py
+++ utils/generator.py
@@ -2,21 +2,16 @@ import sys
 
 sys.path.append(".")
 import os
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import dotenv
-from PIL import Image
-from rich.console import Console
-from rich.progress import Progress, track
-from rich.prompt import Prompt
 
 import database
 import leaderboards
 from utils.raise_for_missing_env import raise_for_missing_env_vars
+from PIL import Image
 
 dotenv.load_dotenv()
 
-console = Console()
 dba = database.DatabaseAccess(
     host=os.getenv("MYSQLHOST") or raise_for_missing_env_vars(),
     user=os.getenv("MYSQLUSER") or raise_for_missing_env_vars(),
@@ -31,16 +26,16 @@ def worker(file: str):
     role = leaderboards.Role.by_name(role)
     region = leaderboards.Region.by_name(region)
 
-    results = leaderboards.parse(  # parse the leaderboard
-        image_path=os.path.join("./assets/leaderboard_images", file),
-        assets_path="./assets/hero_images",
-        role=role,
-        region=region,
-        model_name=model_name,
-    )
+    results = leaderboards.parse_leaderboard_to_leaderboard_entries(
+            leaderboard_image=Image.open(f"./assets/leaderboard_images/{file}"),
+            region=region, # type: ignore
+            role=role, # type: ignore
+            model_name="thearyadev-initial-15-02-2024",
+        )
+
 
     for i in results:
-        if i.heroes[0].name != "Blank":
+        if i.heroes[0] != "Blank":
             dba.add_leaderboard_entry(seasonNumber=target_season, leaderboard_entry=i)
 
 
@@ -48,27 +43,15 @@ def main():
     global target_season, model_name  # globals so the worker threads can access them
     # sorry
 
-    target_season = "8_8"
-    model_name = "thearyadev-2023-12-20"
+    target_season = "9"
     dba.create_season(seasonNumber=target_season)
 
     files = os.listdir("./assets/leaderboard_images")
-    max_workers = 1
-    progress = Progress()
 
-    with progress:
-        progress_bar = progress.add_task(
-            "[red]Parsing Leaderboard Images...", total=len(files)
-        )
-        for file in files:
-            worker(file)
-
-        # with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        #     futures = [executor.submit(worker, file) for file in files]
-        #     for future in as_completed(futures):
-        #         progress.advance(progress_bar)
+    for file in files:
+        worker(file)
 
 
 if __name__ == "__main__":
-    dba.add_info_entry("season_8_8", None, None )
-    main()
+    dba.add_info_entry("season_9", "This season is a test of the new top 500 hero classifier model. There may be issues with this page. This dataset was recorded on Febuary 29, 2AM EDT.", None )
+    # main()
