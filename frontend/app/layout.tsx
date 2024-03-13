406fb50d90708ad5d6e282760d926f1618d59192
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-03-13 11:08:38 -0400

    add mantine dependencies

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import '@mantine/core/styles.css';
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import { Footer, Header } from "@/app/components";
