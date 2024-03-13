b452687460a1bf0d30d1caea3faa65ce28bd59bc
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-03-13 12:50:15 -0400

    Add meta tags

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,7 +5,6 @@ import { ColorSchemeScript, createTheme, MantineProvider } from "@mantine/core";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
-
 import { Footer, Header, ScrollToTop } from "@/app/components";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next"
@@ -28,6 +27,8 @@ export default function RootLayout({
     return (
         <html lang="en">
             <Head>
+                <meta property="twitter:title" content="Overwatch 2: Top 500 Data" />
+                <meta property="twitter:description" content="Data collected from the Overwatch 2 Top 500 Leaderboards" />
                 <ColorSchemeScript />
             </Head>
             <body>
