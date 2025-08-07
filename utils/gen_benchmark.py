fc9f9ffd8f9ced48c30d64a69af4471cf808236d
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2025-08-07 16:26:58 -0400

    fix: replace s17 data

--- utils/gen_benchmark.py
+++ utils/gen_benchmark.py
@@ -6,7 +6,7 @@ sys.path.append("./")  # used to import from root directory
 import leaderboards
 
 R = leaderboards.parse_leaderboard_to_leaderboard_entries(  # parse leaderboard
-                leaderboard_image=Image.open("./assets/benchmark/DAMAGE_S16_P20_AMERICAS/LB-IMG.png"),
+                leaderboard_image=Image.open("./assets/benchmark/TANK_S17_P24_ASIA/LB-IMG.png"),
                 region=leaderboards.Region.AMERICAS,  # doesnt matter
                 role=leaderboards.Role.DAMAGE,  # doesnt matter
             )
